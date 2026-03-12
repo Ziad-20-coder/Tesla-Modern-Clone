b5a9a8ba927d09cb0aadf084b14e28013f994ecb
Ziad <ziad@gmail.com>
2026-03-12 18:00:12 +0200

    added links that located at the end of the website

---
+++ src/Components/EndOfWebsiteLinks.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const EndOfWebsiteLinks = () => {
+  return (
+    <div className='mt-9 flex flex-col md:flex-row items-center justify-center gap-5 text-sm text-black/70 cursor-pointer'>
+        <p><a href="#">Tesla&copy;2026</a></p>
+        <p><a href="#">Privacy & Legal</a></p>
+        <p><a href="#">Vehicle Recalls</a></p>
+        <p><a href="#">News</a></p>
+        <p><a href="#">Learn</a></p>
+    </div>
+  )
+}
+
+export default EndOfWebsiteLinks
